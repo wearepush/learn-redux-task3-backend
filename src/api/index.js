841787a4e049906c3f36fdc0c51efbe57ba5c3b6
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2019-11-14 20:02:31 +0200

    update mock data

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ import userId1 from '../fakeData/userId1';
 import facets from './facets';
 
 function checkCredentials(body) {
-	if (body.email === 'max@test.com' && body.password === '12345') {
+	if (body.email === 'admin@test.com' && body.password === '12345') {
 		return true
 	}
 	return false
