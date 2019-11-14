f27ccecb4903828ef495166d7ce32e9869a88634
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2019-11-14 19:26:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,56 @@
+# Backend для тестового задания #3
+
+### Поддерживаемые маршруты:
+
+```js
+// Корневой адрес API: http://localhost:8080/api/v1
+
+// POST /validate (введены корректные данные: username = 'max', password = '12345'
+{
+  "status": "ok",
+  "data": {
+    "id": 1
+  }
+}
+
+// POST /validate (введены некорректные данные)
+{
+  "status": "err",
+  "message": "wrong_email_or_password"
+}
+
+// GET /user-info/:id - где, id - идентификатор пользователя
+// GET /user-info/1
+{
+  "status": "ok",
+  "data": {
+    "id": 1,
+  }
+}
+
+// GET /user-info/2
+{
+  "status": "err",
+  "message": "user_not_found"
+}
+
+// GET /news
+{
+  "status": "ok",
+  "data": [
+  ]
+}
+```
+
+### Запуск
+
+```sh
+# Install dependencies
+npm install
+
+# Start development live-reload server
+npm run dev
+
+# Start production server:
+npm start
+```
---
+++ src/api/index.js
@@ -0,0 +1,54 @@
+import { version } from '../../package.json';
+import { Router } from 'express';
+import news from '../fakeData/news';
+import userId1 from '../fakeData/userId1';
+import facets from './facets';
+
+function checkCredentials(body) {
+	if (body.email === 'max@test.com' && body.password === '12345') {
+		return true
+	}
+	return false
+}
+
+export default ({ config, db }) => {
+	let api = Router();
+
+	// mount the facets resource
+	api.use('/facets', facets({ config, db }));
+
+	api.get('/', (req, res) => {
+		res.json({ version });
+	});
+
+	api.post('/validate', (req, res) => {
+		if (checkCredentials(req.body)) {
+			return res.json({
+				"status": "ok",
+				"data": {
+					"id": 1
+				}
+			})
+		}
+		res.json({
+			"status": "err",
+			"message": "wrong_email_or_password"
+		})
+	})
+
+	api.get('/news', (req, res) => {
+		res.json(news);
+	})
+
+	api.get('/user-info/:id', (req, res) => {
+		if (parseInt(req.params.id) === 1) {
+			return res.json(userId1);
+		}
+		res.json({
+			"status": "err",
+			"message": "user_not_found",
+		})
+	})
+
+	return api;
+}
